9da241869767b0bf4b549b12273cb078f712a66f
KwonOhGeun <ohgeun72@naver.com>
2022-07-04 14:09:12 +0900

    Initial commit

---
+++ src/main/java/kopo/poly/controller/NoticeController.java
@@ -0,0 +1,53 @@
+package kopo.poly.controller;
+
+import kopo.poly.util.CmmUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@Controller
+public class NoticeController {
+
+    @GetMapping(value = "index")
+    public String indexPage() throws Exception {
+        log.info(this.getClass().getName() + " .indexPage Start !!");
+        log.info(this.getClass().getName() + " .indexPage End !!");
+
+        return "index";
+    }
+
+    @GetMapping(value = "noticeInfo")
+    public String noticeInfo() throws Exception {
+        log.info(this.getClass().getName() + " .indexPage Start !!");
+        log.info(this.getClass().getName() + " .indexPage End !!");
+
+        return "form";
+    }
+
+    @RequestMapping(value = "getNoticeData", method = {RequestMethod.GET,RequestMethod.POST})
+    public String getNoticeData(HttpServletRequest request, Model model) throws Exception {
+        log.info(this.getClass().getName() + ".getNoticeData Start !!");
+        String title = CmmUtil.nvl(request.getParameter("title"));
+        String contents = CmmUtil.nvl(request.getParameter("contents"));
+
+        log.info("title : " + title);
+        log.info("content : " + contents);
+
+        model.addAttribute("title", title);
+        model.addAttribute("contents", contents);
+
+        log.info(this.getClass().getName() + " .getNoticeData End !!");
+        return "getNoticeData";
+
+
+    }
+}
+
+
